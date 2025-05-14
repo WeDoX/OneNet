c395ae2cdd61362c12df11cc2c617df6c2f14be5
jdallen <1986821027@qq.com>
2025-05-14 16:58:05 +0800

    发布1.3.0
    支持http头部value的值可以动态化，比如Token

--- app/src/main/java/com/onedream/onenet/http_demo/config/HttpConfig.kt
+++ app/src/main/java/com/onedream/onenet/http_demo/config/HttpConfig.kt
@@ -2,7 +2,7 @@ package com.onedream.onenet.http_demo.config
 
 import android.content.Context
 import com.onedream.onenet.config.OneNetCommonConfig
-import com.onedream.onenet.interceptor.HeaderInterceptor.HeaderProvider
+import com.onedream.onenet.interceptor.HeaderInterceptor
 
 /**
  *@author chenguijian
@@ -13,7 +13,7 @@ class HttpConfig(context: Context) : OneNetCommonConfig(context, isDebug = true)
         return HttpApiConstant.BASE_URL
     }
 
-    fun headerProvider(): HeaderInterceptor.HeaderProvider{
+    override fun headerProvider(): HeaderInterceptor.HeaderProvider{
         return object : HeaderInterceptor.HeaderProvider{
             override fun getHeaders(): MutableMap<String, String> {
                 val header = HashMap<String, String>()
